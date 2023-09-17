6ccf26070e44f88937eddfda15c6de7cc9772284
¨ZiskaS¨ <franziska.stude@gmx.de>
2023-09-17 17:42:39 +0200

    insert Timeout in PostList

--- my-dog/src/components/PostList.jsx
+++ my-dog/src/components/PostList.jsx
@@ -1,56 +1,68 @@
-import React from 'react';
-import Post from './Post'; 
+import React, { useState, useEffect } from 'react';
+import Post from './Post';
 
 function PostList({ searchValue }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [filteredPosts, setFilteredPosts] = useState([]);
+
+  useEffect(() => {
+    // Define the 'posts' array inside the useEffect callback
     const posts = [
       {
-      author: 'Elisa',
-      date: new Date(), // Updated to use JavaScript Date object
-      description: 'I love my dog - so cute!',
-      image: 'dog-1.jpg',
-      comments: 22,
-      likes: 99,
+        author: 'Elisa',
+        date: new Date(),
+        description: 'I love my dog - so cute!',
+        image: 'dog-1.jpg',
+        comments: 22,
+        likes: 99,
       },
-  
       {
         author: 'Lucia',
-        date: new Date(), // Updated to use JavaScript Date object
-        description: 'My sweatheart!',
+        date: new Date(),
+        description: 'She is so sweet!',
         image: 'dog-2.jpg',
         comments: 5,
         likes: 10,
-        },
-  
-        {
-          author: 'Ana',
-          date: new Date(), // Updated to use JavaScript Date object
-          description: 'Best of the World!',
-          image: 'dog-3.jpg',
-          comments: 15,
-          likes: 233,
-          }
+      },
+      {
+        author: 'Ana',
+        date: new Date(),
+        description: 'I adore my furry friend!',
+        image: 'dog-3.jpg',
+        comments: 15,
+        likes: 233,
+      },
     ];
-  
-    // Function to filter posts based on the search query
+
     const filterPosts = (query) => {
-    const lowercasedQuery = query.toLowerCase();
-    return posts.filter((post) => {
-      const lowercasedAuthor = post.author.toLowerCase();
-      const lowercasedDescription = post.description.toLowerCase();
-      // Check if the query appears in the author or description fields
-      return (
-        lowercasedAuthor.includes(lowercasedQuery) ||
-        lowercasedDescription.includes(lowercasedQuery)
-      );
-    });
-  };
+      const lowercasedQuery = query.toLowerCase();
+      return posts.filter((post) => {
+        const lowercasedAuthor = post.author.toLowerCase();
+        const lowercasedDescription = post.description.toLowerCase();
+        return (
+          lowercasedAuthor.includes(lowercasedQuery) ||
+          lowercasedDescription.includes(lowercasedQuery)
+        );
+      });
+    };
 
-  const filteredPosts = filterPosts(searchValue);
+    const timer = setTimeout(() => {
+      const filtered = filterPosts(searchValue);
+      setFilteredPosts(filtered);
+      setIsLoading(false);
+    }, 3000);
 
-    return (
-      <div className="container">
-         {filteredPosts.map((post, index) => (
+    return () => clearTimeout(timer);
+  }, [searchValue]);
+
+  return (
+    <div className="container">
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        filteredPosts.map((post, index) => (
           <Post
+            key={index}
             author={post.author}
             date={post.date}
             description={post.description}
@@ -58,9 +70,11 @@ function PostList({ searchValue }) {
             comments={post.comments}
             likes={post.likes}
           />
-        ))}
-      </div>
-    );
-  }
-  
+        ))
+      )}
+    </div>
+  );
+}
+
 export default PostList;
+
